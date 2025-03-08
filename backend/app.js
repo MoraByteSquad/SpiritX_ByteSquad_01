2aea0265f93d198d742fe0f5438eb974c0382ebd
Suraja Hasarinda <surajahasarinda@gmail.com>
2025-03-08 16:20:05 +0530

    implement CORS in backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config/env.js';
 import connectDB from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
@@ -9,6 +10,7 @@ import authRouter from './routes/auth.routes.js';
 const app = express();
 
 // Middleware
+app.use(cors({ origin: '*', credentials: true })); // Allow all origins
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
 app.use(cookieParser());
