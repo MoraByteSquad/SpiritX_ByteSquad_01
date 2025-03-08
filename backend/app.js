6826f376cb4fcfe0ef22d05d92c9990cc3658fa8
Suraja Hasarinda <surajahasarinda@gmail.com>
2025-03-08 16:00:08 +0530

    Error response set to JSON format

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
 app.use(cookieParser());
-app.use(errorMiddleware); // Error handling middleware
 
 // Routes
 app.use('/api/v1/auth', authRouter);
@@ -20,6 +19,9 @@ app.use('/api/v1/auth', authRouter);
 app.get('/', (req, res) => {
   res.send('SpritX 2025 project by ByteSquad');
 });
+
+// Error handling middleware should be the last middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
